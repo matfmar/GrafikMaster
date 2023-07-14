e758eeb744abad1c4153ccaf83dfa3118904e30f
Mateusz Marzec <marzec-mateusz@wp.pl>
2023-07-14 19:52:04 +0200

    Update pprzegladaniegrafikowroboczych.h

--- GrafikMaster/pprzegladaniegrafikowroboczych.h
+++ GrafikMaster/pprzegladaniegrafikowroboczych.h
@@ -10,6 +10,10 @@ class PPrzegladanieGrafikowRoboczych
 public:
     PPrzegladanieGrafikowRoboczych();
     ~PPrzegladanieGrafikowRoboczych();
+    void wybranoSzukanieGrafikow(int miesiac, int rok);
+    void wybranoGrafikWLewo();
+    void wybranoGrafikWPrawo();
+    void wybranoZamkniecie();
 private:
     UIPrzegladanieGrafikowRoboczych* uiPrzegladanieGrafikowRoboczych;
 };
