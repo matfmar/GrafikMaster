6b74c496f11e3cf54265d7000358810dff38e54d
Mateusz Marzec <marzec-mateusz@wp.pl>
2023-07-14 19:38:36 +0200

    Update uiprzegladaniegrafikowroboczych.cpp

--- GrafikMaster/uiprzegladaniegrafikowroboczych.cpp
+++ GrafikMaster/uiprzegladaniegrafikowroboczych.cpp
@@ -8,7 +8,19 @@ UIPrzegladanieGrafikowRoboczych::UIPrzegladanieGrafikowRoboczych(PPrzegladanieGr
     pPrzegladanieGrafikowRoboczych = p;
     mainWidget = new QWidget(this);
 
-        
+    labelMiesiac = new QLabel(tr("Miesiąc:"), this);
+    labelRok = new QLabel(tr("Rok":), this);
+    editMiesiac = new QLineEdit(this);
+    editRok = new QLineEdit(this);
+    buttonSzukaj = new QPushButton(tr("Szukaj grafików"), this);
+    groupMiesiacRok = new QGroupBox(tr("Wyszukiwanie grup grafików"), this);
+    layoutGroup = new QGridLayout(this);
+    layoutGroup->addWidget(labelMiesiac, 0, 0);
+    layoutGroup->addWidget(editMiesiac, 0, 1);
+    layoutGroup->addWidget(labelRok, 1, 0);
+    layoutGroup->addWidget(editRok, 1, 1);
+    layoutGroup->addWidget(buttonSzukaj, 2, 0);
+    groupMiesiacRok->setLayout(layoutGroup);
 
 /*
     PPrzegladanieGrafikowRoboczych* pPrzegladanieGrafikowRoboczych;
