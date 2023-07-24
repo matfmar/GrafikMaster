d98a5568467f812ff276bb7f8b9a3204c7faa9ef
Mateusz Marzec <marzec-mateusz@wp.pl>
2023-07-24 22:57:29 +0200

    Update pdodawanienowegografiku.h

--- GrafikMaster/pdodawanienowegografiku.h
+++ GrafikMaster/pdodawanienowegografiku.h
@@ -27,7 +27,7 @@ public:
     XDyzurantTworzacy* wybranoPobranieDanychDyzurantaTworzacego(QString nick);
     bool wybranoUpdateDyzurantaTworzacego(QString nick, bool czyM, QString m, QString nm, QString u, QString ch, int maks, int min, int maksS, int maksN, int maksW, int c, int maksP, int wyborT);
 
-    void wybranoProsbeOStworzenieGrafiku(bool& immediateResult, int ileIteracji, int szybkosc);
+    void wybranoProsbeOStworzenieGrafiku(bool& immediateResult, int ileIteracji, int szybkosc, bool skracaniePomimoUlozenia);
 
     void wybranoDecyzjeWarunkowPoczatkowychGrafiku(int decyzja);
     bool wybranoZapisanieUstawienDoPliku();
